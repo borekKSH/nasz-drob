e8c9791ea53f9aeed8e07483203fb44039146981
borekKSH <39568316+borekKSH@users.noreply.github.com>
2024-03-22 12:29:17 +0100

    modified:   components/Navbar.tsx 	renamed:    public/nasz.png -> public/logo.png

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,83 +1,127 @@
-'use client'
-import { Flex, Box, Text, useColorModeValue, Link, Menu, MenuButton, MenuList, MenuItem, Image } from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import { link } from 'fs';
+"use client"
+import React from 'react';
+import {
+  Flex, Box, Text, Link, Menu, MenuButton, MenuList, MenuItem,
+  useDisclosure, Collapse, IconButton, useColorModeValue
+} from '@chakra-ui/react';
+import { ChevronDownIcon, HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
 export default function Navbar() {
-  // Styl dla boxów z przezroczystym tłem
-  const boxBgStyle = {
-    bg: useColorModeValue('rgba(255, 255, 255, 0.6)', 'rgba(23, 25, 35, 0.6)'), // przezroczystość dla jasnego i ciemnego motywu
-    backdropFilter: 'blur(10px)', // efekt rozmycia dla tła
-    p: '0.5rem', // padding wewnątrz boxów
-    borderRadius: 'lg', // zaokrąglenie rogów
-    boxShadow: 'md' // delikatny cień dla lepszego kontrastu
+  const { isOpen, onToggle } = useDisclosure();
+  const navItemBg = useColorModeValue('rgba(255, 255, 255, 0.6)', 'rgba(23, 25, 35, 0.6)');
+  const navItemBgHover = useColorModeValue('rgba(255, 255, 255, 0.8)', 'rgba(23, 25, 35, 0.8)');
+
+  const menuItemStyle = {
+    mr: 4,
+    padding: '0.5rem',
+    borderRadius: 'lg',
+    boxShadow: 'md',
+    bg: navItemBg,
+    color: 'black',
+    fontWeight: 'bold',
+    _hover: {
+      bg: navItemBgHover,
+    },
   };
 
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="start"
-      wrap="wrap"
-      padding="1.5rem"
-      bg="transparent"
-      color={useColorModeValue('gray.600', 'white')}
-      position="absolute"
-      width="100%"
-      zIndex="10"
-    >
-      {/* Zastosowanie stylu tła dla logo */}
-      <Box mr={10} mt={0} {...boxBgStyle}>
-        <Link href="/" _hover={{ textDecoration: 'none' }}>
-          <Image src="/nasz.png" alt="NaszDrob" height={250}/>
-        </Link>
-      </Box>
+    <Box>
+      <Flex
+        as="nav"
+        align="center"
+        justify={{ base: 'center', md: 'start' }}
+        wrap="wrap"
+        padding="1.5rem"
+        bg="transparent"
+        color="black"
+        position="absolute"
+        width="100%"
+        zIndex="10"
+      >
+        <Box {...menuItemStyle}>
+          <Link href="/" _hover={{ textDecoration: 'none' }}>
+            <Text fontSize="2xl" fontWeight="bold">
+              NaszDrób
+            </Text>
+          </Link>
+        </Box>
 
-      <Box {...boxBgStyle}>
-        <Menu>
-          <MenuButton as={Text} mr={4} cursor="pointer" fontSize="lg" _hover={{ textDecoration: 'underline' }} onMouseEnter={(event) => openMenu(event)}>
-            Firma <ChevronDownIcon />
-          </MenuButton>
-          <MenuList>
-            <MenuItem as={Link} href="/o-nas">O nas</MenuItem>
-          </MenuList>
-        </Menu>
-      </Box>
+        <Flex display={{ base: 'none', md: 'flex' }} flexGrow={1} align="center">
+          <MenuItems menuItemStyle={menuItemStyle} />
+        </Flex>
 
-      <Box {...boxBgStyle}>
-        <Menu>
-          <MenuButton as={Link} mr={4} href="/produkty" fontSize="lg">
-            Produkty
-          </MenuButton>
-        </Menu>
-      </Box>
+        <IconButton
+          display={{ base: 'flex', md: 'none' }}
+          onClick={onToggle}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          variant="outline"
+          aria-label="Toggle Navigation"
+          size="lg"
+        />
 
-      <Box {...boxBgStyle}>
-        <Menu>
-          <MenuButton as={Text} mr={4} cursor="pointer" fontSize="lg" _hover={{ textDecoration: 'underline' }} onMouseEnter={(event) => openMenu(event)}>
-            Informacje <ChevronDownIcon />
-          </MenuButton>
-          <MenuList>
-            <MenuItem as={Link} href="/haccp">System HACCP</MenuItem>
-            <MenuItem as={Link} href="/certyfikaty">Certyfikaty</MenuItem>
-          </MenuList>
-        </Menu>
-      </Box>
-
-      <Box {...boxBgStyle}>
-        <Menu>
-          <MenuButton as={Link} mr={4} href="/kontakt" fontSize="lg">
-            Kontakt
-          </MenuButton>
-        </Menu>
-      </Box>
-    </Flex>
+        <Collapse in={isOpen} animateOpacity>
+          <Box pb={4} display={{ md: 'none' }} width="100%">
+            <MenuItems menuItemStyle={menuItemStyle} />
+          </Box>
+        </Collapse>
+      </Flex>
+    </Box>
   );
 }
 
-function openMenu(event: React.MouseEvent) {
-  let menuList = (event.target as HTMLElement).parentNode?.querySelector('ul');
-  if (menuList) {
-    menuList.style.display = menuList.style.display === 'block' ? 'none' : 'block';
-  }
+function MenuItems({ menuItemStyle }: MenuItemsProps) {
+  return (
+    <>
+      <Menu>
+        <MenuButton as={Link} href="#" style={menuItemStyle}>
+          Firma <ChevronDownIcon />
+        </MenuButton>
+        <MenuList style={menuItemStyle}>
+          <MenuItem as={Link} href="/o-nas" _hover={menuItemStyle._hover}>
+            O nas
+          </MenuItem>
+          <MenuItem as={Link} href="/certyfikaty" _hover={menuItemStyle._hover}>
+            Certyfikaty
+          </MenuItem>
+        </MenuList>
+      </Menu>
+
+      <Box as={Link} href="/produkty" style={menuItemStyle}>
+        Produkty
+      </Box>
+
+      <Menu>
+        <MenuButton as={Link} href="#" style={menuItemStyle}>
+          Informacje <ChevronDownIcon />
+        </MenuButton>
+        <MenuList style={menuItemStyle}>
+          <MenuItem as={Link} href="/haccp" _hover={menuItemStyle._hover}>
+            System HACCP
+          </MenuItem>
+          <MenuItem as={Link} href="/certyfikaty" _hover={menuItemStyle._hover}>
+            Certyfikaty
+          </MenuItem>
+        </MenuList>
+      </Menu>
+
+      <Box as={Link} href="/kontakt" style={menuItemStyle}>
+        Kontakt
+      </Box>
+    </>
+  );
+}
+
+interface MenuItemsProps {
+  menuItemStyle: {
+    mr: number;
+    padding: string;
+    borderRadius: string;
+    boxShadow: string;
+    bg: string;
+    color: string;
+    fontWeight: string;
+    _hover: {
+      bg: string;
+    };
+  };
 }
